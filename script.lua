1146bb1d233888dd242b72f813420b6d43505987
Paule1248 <161169889+Paule1248@users.noreply.github.com>
2024-02-25 00:23:10 +0100

    script.lua

--- script.lua
+++ script.lua
@@ -5,6 +5,9 @@ local Window = OrionLib:MakeWindow({Name = "PS99", HidePremium = false, IntroEna
 _G.autohatch = true
 _G.SelectEgg = "Tech Ciruit Egg"
 _G.EggAmount = 1
+_G.autoconsumeFruits = true
+
+
 
 --Functions
 
@@ -14,13 +17,28 @@ function autohatch()
             wait(.0001)
         end
     end
-
-function AutoTeleportToEgg()
-    while
-
+--consume Fruits
+function autoconsumeFruits()
+    while _G.autoconsumeFruits == true do      
+        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("bea094be6eac4024a969dbf1ec436330",1)
+        wait(0.1)
+        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("25c53be530b54728b97be1b81720a929",1)
+        wait(0.1)
+        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("5cc6a6e2acb94467a92560e10b37f83e",1)
+        wait(0.1)
+        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("a7408cf5fad4490a9a9b8ff5d379a392",1)    
+        wait(0.1)
+        end
+    end
+        
 --Tabs
 local EggTab = Window:MakeTab({
 	Name = "Eggs",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local ItemsTab = Window:MakeTab({
+	Name = "Items",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -28,6 +46,11 @@ local MiscTab = Window:MakeTab({
 	Name = "Misc",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+
+--sections
+local Section = ItemsTab:AddSection({
+	Name = "Auto Eat Fruits"
 })
 
 --slider
@@ -46,7 +69,6 @@ EggTab:AddSlider({
 })
 
 --dropdowns
-
 EggTab:AddDropdown({
 	Name = "Select egg",
 	Default = "Tech Ciruit Egg",
@@ -56,7 +78,15 @@ EggTab:AddDropdown({
         print(_G.SelectEgg)
 	end    
 })
-
+ItemsTab:AddDropdown({
+	Name = "Select egg",
+	Default = "Tech Ciruit Egg",
+	Options = {"Tech Ciruit Egg", "Tech City Egg"},
+	Callback = function(Value)
+		_G.SelectEgg = Value
+        print(_G.SelectEgg)
+	end    
+})
 
 --Toogles
 EggTab:AddToggle({
@@ -67,8 +97,15 @@ EggTab:AddToggle({
 		autohatch()
 	end
 })
-
-
+--AutoConsume
+ItemsTab:AddToggle({
+	Name = "Eat all Fruits",
+	Default = false,
+	Callback = function(Value)
+        _G.autoconsumeFruits = Value
+        autoconsumeFruits()
+	end
+})
 
 --buttons
 EggTab:AddButton({
@@ -83,3 +120,4 @@ MiscTab:AddButton({
         loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
   	end    
 })
+
