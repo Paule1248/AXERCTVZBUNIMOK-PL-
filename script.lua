8409e9bdbecf552838d2d67532f7e95962fb1c34
Paule1248 <161169889+Paule1248@users.noreply.github.com>
2024-02-25 13:23:50 +0100

    script.lua

--- script.lua
+++ script.lua
@@ -3,44 +3,112 @@ local Window = OrionLib:MakeWindow({Name = "PS99", HidePremium = false, IntroEna
 
 --Values
 _G.autohatch = true
+_G.autohatchMobile = true
 _G.SelectEgg = "Tech Ciruit Egg"
 _G.EggAmount = 1
-_G.autoconsumeFruits = true
+_G.EggAmountMobile = 1
 _G.UltimateSelected = "Ground Pound"
 _G.autoUltimateuse = true
-_G.autoMagnetFlag = true
+_G.WebhookUse = ""
+_G.Webhookstats = true
+_G.webhookwait = 30
+local WebhookUse = _G.WebhookUse
 
 --Functions
-
 function autohatch()
     while _G.autohatch == true do
             game:GetService("ReplicatedStorage").Network.Eggs_RequestPurchase:InvokeServer(_G.SelectEgg,_G.EggAmount)
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-10043.3134765625, 16.506406784057617, -314.23333740234375)
             wait(.0001)
         end
     end
-
-function autoconsumeFruits()
-    while _G.autoconsumeFruits == true do      
-        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("bea094be6eac4024a969dbf1ec436330",1)
-        wait(0.1)
-        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("25c53be530b54728b97be1b81720a929",1)
-        wait(0.1)
-        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("5cc6a6e2acb94467a92560e10b37f83e",1)
-        wait(0.1)
-        game:GetService("ReplicatedStorage").Network["Fruits: Consume"]:FireServer("a7408cf5fad4490a9a9b8ff5d379a392",1)    
-        wait(0.1)
-        end
-    end
+function autohatchMobile()
+		while _G.autohatch == true do
+				game:GetService("ReplicatedStorage").Network.Eggs_RequestPurchase:InvokeServer(_G.SelectEgg,_G.EggAmountMobile)
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-10043.3134765625, 16.506406784057617, -314.23333740234375)
+				wait(.0001)
+			end
+		end
+	
 function autoUltimate()
     while _G.autoUltimateuse == true do
             game:GetService("ReplicatedStorage").Network["Ultimates: Activate"]:InvokeServer(_G.UltimateSelected)
             wait(.05)
         end
     end
-function magnetflag()
-	while _G.autoMagnetFlag == true do
+function sendWebhook_stats()
+	while _G.Webhookstats == true do
+		function SendMessage(url, message)
+			local http = game:GetService("HttpService")
+			local headers = {
+				["Content-Type"] = "application/json"
+			}
+			local data = {
+				["content"] = message
+			}
+			local body = http:JSONEncode(data)
+			local response = request({
+				Url = url,
+				Method = "POST",
+				Headers = headers,
+				Body = body
+			})
+		end
 		
-		wait(0.001)
+		function SendMessageEMBED(url, embed)
+			local http = game:GetService("HttpService")
+			local headers = {
+				["Content-Type"] = "application/json"
+			}
+			local data = {
+				["embeds"] = {
+					{
+						["title"] = embed.title,
+						["description"] = embed.description,
+						["color"] = embed.color,
+						["fields"] = embed.fields,
+						["footer"] = {
+							["text"] = embed.footer.text
+						}
+					}
+				}
+			}
+			local body = http:JSONEncode(data)
+			local response = request({
+				Url = url,
+				Method = "POST",
+				Headers = headers,
+				Body = body
+			})
+		end
+		
+		
+		--Examples 
+		local url = _G.WebhookUse
+		SendMessage(url, "Hello")
+		
+		
+		local embed = {
+			["title"] = "Test",
+			--["description"] = "Test Desc",
+			["color"] = 65280,
+			["fields"] = {
+				{
+					["name"] = "Stats",
+					["value"] = "AA"
+				},
+				--{
+					--["name"] = "Field 2",
+					--["value"] = "This is the second field"
+				--}
+			},
+			["footer"] = {
+				["text"] = "LOL"
+			}
+		}
+		SendMessageEMBED(url, embed)
+		wait(30)
+
 	end
 end
 --Tabs
@@ -51,11 +119,6 @@ local MainTab = Window:MakeTab({
 })
 local EggTab = Window:MakeTab({
 	Name = "Eggs",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local ItemsTab = Window:MakeTab({
-	Name = "Items",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -71,9 +134,6 @@ local WebhookTab = Window:MakeTab({
 })
 
 --sections
-local Section = ItemsTab:AddSection({
-	Name = "Auto Eat Fruits"
-})
 
 --slider
 
@@ -117,6 +177,22 @@ EggTab:AddToggle({
 		autohatch()
 	end
 })
+EggTab:AddToggle({
+	Name = "AutoHatch (Mobile)",
+	Default = false,
+	Callback = function(Value)
+        _G.autohatchMobile = Value
+		autohatchMobile()
+	end
+})
+WebhookTab:AddToggle({
+	Name = "StatWebhook",
+	Default = false,
+	Callback = function(Value)
+        _G.Webhookstats = Value
+		sendWebhook_stats()
+	end
+})
 MainTab:AddToggle({
 	Name = "Auto Use Ultimate",
 	Default = false,
@@ -125,24 +201,6 @@ MainTab:AddToggle({
 		autoUltimate()
 	end
 })
-MainTab:AddToggle({
-	Name = "Auto Magnet Flag",
-	Default = false,
-	Callback = function(Value)
-        _G.autoMagnetFlag = Value
-		magnetflag()
-	end
-})
---AutoConsume
-ItemsTab:AddToggle({
-	Name = "Eat all Fruits",
-	Default = false,
-	Callback = function(Value)
-        _G.autoconsumeFruits = Value
-        autoconsumeFruits()
-	end
-})
-
 --buttons
 EggTab:AddButton({
 	Name = "Desable Egg animations",
@@ -157,12 +215,28 @@ MiscTab:AddButton({
   	end    
 })
 
---Adaptive Input 
+--Adaptive Input
 EggTab:AddTextbox({
 	Name = "Amount Eggs (Mobile)",
 	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.EggAmountMobile = Value
+	end	  
+})
+WebhookTab:AddTextbox({
+	Name = "Input Webhook",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
-		_G.EggAmount = Value
+		_G.WebhookUse = Value
+	end	  
+})
+WebhookTab:AddTextbox({
+	Name = "Webhook delay",
+	Default = "30",
+	TextDisappear = false,
+	Callback = function(Value)
+		_G.webhookwait = Value
 	end	  
 })
