04f7eeadad8ea05ac339b4d0e609902280e65a1a
Paule1248 <161169889+Paule1248@users.noreply.github.com>
2024-02-25 17:06:10 +0100

    script.lua

--- script.lua
+++ script.lua
@@ -1,16 +1,28 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "PS99", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "Ps99"})
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "PS99 Autohatch " .. Fluent.Version,
+    SubTitle = "by Yusei",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Misc = Window:AddTab({ Title = "Misc", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
 
 --Values
 _G.autohatch = true
 _G.SelectEgg = "Tech Ciruit Egg"
 _G.EggAmount = 1
-_G.UltimateSelected = "Ground Pound"
-_G.autoUltimateuse = true
-_G.WebhookUse = ""
-_G.Webhookstats = true
-_G.webhookwait = 30
-local WebhookUse = _G.WebhookUse
 
 --Functions
 function autohatch()
@@ -20,197 +32,84 @@ function autohatch()
             wait(.0001)
         end
     end
-function autoUltimate()
-    while _G.autoUltimateuse == true do
-            game:GetService("ReplicatedStorage").Network["Ultimates: Activate"]:InvokeServer(_G.UltimateSelected)
-            wait(.05)
-        end
-    end
-function sendWebhook_stats()
-	while _G.Webhookstats == true do
-		function SendMessage(url, message)
-			local http = game:GetService("HttpService")
-			local headers = {
-				["Content-Type"] = "application/json"
-			}
-			local data = {
-				["content"] = message
-			}
-			local body = http:JSONEncode(data)
-			local response = request({
-				Url = url,
-				Method = "POST",
-				Headers = headers,
-				Body = body
-			})
-		end
-		
-		function SendMessageEMBED(url, embed)
-			local http = game:GetService("HttpService")
-			local headers = {
-				["Content-Type"] = "application/json"
-			}
-			local data = {
-				["embeds"] = {
-					{
-						["title"] = embed.title,
-						["description"] = embed.description,
-						["color"] = embed.color,
-						["fields"] = embed.fields,
-						["footer"] = {
-							["text"] = embed.footer.text
-						}
-					}
-				}
-			}
-			local body = http:JSONEncode(data)
-			local response = request({
-				Url = url,
-				Method = "POST",
-				Headers = headers,
-				Body = body
-			})
-		end
-		
-		
-		--Examples 
-		local url = _G.WebhookUse
-		SendMessage(url, "Hello")
-		
-		
-		local embed = {
-			["title"] = "Test",
-			--["description"] = "Test Desc",
-			["color"] = 65280,
-			["fields"] = {
-				{
-					["name"] = "Stats",
-					["value"] = "AA"
-				},
-				--{
-					--["name"] = "Field 2",
-					--["value"] = "This is the second field"
-				--}
-			},
-			["footer"] = {
-				["text"] = "LOL"
-			}
-		}
-		SendMessageEMBED(url, embed)
-		wait(30)
 
-	end
-end
---Tabs
-local MainTab = Window:MakeTab({
-	Name = "MainFarm",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local EggTab = Window:MakeTab({
-	Name = "Eggs",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local MiscTab = Window:MakeTab({
-	Name = "Misc",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local WebhookTab = Window:MakeTab({
-	Name = "Webhook",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
 
---sections
+--toggle
+local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Auto Hatch", Default = false })
+Toggle:OnChanged(function(Value)
+    _G.autohatch = Value
+	autohatch()
+end)
+
+--dropdown
+local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+    Title = "Dropdown",
+    Values = {"Tech Ciruit Egg"},
+    Multi = false,
+    Default = 1,
+})
+Dropdown:OnChanged(function(Value)
+    _G.UltimateSelected = Value
+end)
+
+
 
 --slider
-
-EggTab:AddSlider({
-	Name = "Egg Amount",
-	Min = 1,
-	Max = 99,
-	Default = 1,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "EggAmount",
-	Callback = function(Value)
-		_G.EggAmount = Value
-	end    
+local Slider = Tabs.Main:AddSlider("Slider", {
+    Title = "Select Egg",
+    Description = "Eggs",
+    Default = 1,
+    Min = 1,
+    Max = 99,
+    Rounding = 0,
+    Callback = function(Value)
+        _G.EggAmount = Value
+    end
 })
 
---dropdowns
-MainTab:AddDropdown({
-	Name = "Select Ultimate",
-	Default = "Ground Pound",
-	Options = {"Ground Pound", "Black Hole", "Chest Spell", "Tornado", "Pet Surge", "TNT Shower"},
-	Callback = function(Value)
-		_G.UltimateSelected = Value
-	end    
-})
-EggTab:AddDropdown({
-	Name = "Select Egg",
-	Default = "Tech Ciruit Egg",
-	Options = {"Tech Ciruit Egg", "Tech City Egg"},
-	Callback = function(Value)
-		_G.SelectEgg = Value
-	end    
-})
 
---Toogles
-EggTab:AddToggle({
-	Name = "AutoHatch (PC)",
-	Default = false,
-	Callback = function(Value)
-        _G.autohatch = Value
-		autohatch()
-	end
-})
-WebhookTab:AddToggle({
-	Name = "StatWebhook",
-	Default = false,
-	Callback = function(Value)
-        _G.Webhookstats = Value
-		sendWebhook_stats()
-	end
-})
-MainTab:AddToggle({
-	Name = "Auto Use Ultimate",
-	Default = false,
-	Callback = function(Value)
-        _G.autoUltimateuse = Value
-		autoUltimate()
-	end
-})
---buttons
-EggTab:AddButton({
-	Name = "Desable Egg animations",
-	Callback = function()
+Tabs.Misc:AddButton({
+    Title = "Remove Egg Animation",
+    Description = "Very important button",
+    Callback = function()
         local Eggs = game.Players.LocalPlayer.PlayerScripts.Scripts.Game['Egg Opening Frontend']getsenv(Eggs).PlayEggAnimation = function() return end
-  	end    
+    end
 })
-MiscTab:AddButton({
-	Name = "rejoin current server",
-	Callback = function()
+Tabs.Misc:AddButton({
+    Title = "rejoin current server",
+    Description = "Very important button",
+    Callback = function()
         loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
-  	end    
+    end
+})
+Tabs.Misc:AddButton({
+    Title = "Safe settings",
+    Description = "Very important button",
+    Callback = function()
+        SaveManager:SetLibrary(Fluent)
+        SaveManager:SetFolder("FluentScriptHub/specific-game")
+    end
 })
 
---Adaptive Input
-WebhookTab:AddTextbox({
-	Name = "Input Webhook",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		_G.WebhookUse = Value
-	end	  
+
+
+
+
+
+
+
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+Window:SelectTab(1)
+Fluent:Notify({
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
 })
-WebhookTab:AddTextbox({
-	Name = "Webhook delay",
-	Default = "30",
-	TextDisappear = false,
-	Callback = function(Value)
-		_G.webhookwait = Value
-	end	  
-})
+SaveManager:LoadAutoloadConfig()
